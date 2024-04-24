438fea3ac150b75fa076170be6379daed416ef89
devpnThunder <devpthunder@gmail.com>
2024-04-24 16:15:38 +0300

    Task #1 commit #6

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install puppet-lint
-package{'puppet-lint':
-ensure   => '2.5.0',
-provider => 'gem'
+# Install the puppet-lint package
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem',
 }
