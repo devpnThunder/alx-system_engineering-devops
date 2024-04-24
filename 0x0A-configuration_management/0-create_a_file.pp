a12611b3cb9745cd535618e43e8b986190ac15f2
devpnThunder <devpthunder@gmail.com>
2024-04-24 15:55:50 +0300

    Task #0 commit #7

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
 # Create a file in /tmp
-file {'/tmp/school':
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
+file {'school':
+ensure	=> 'present',
+path	=> '/tmp/school',
+mode	=> '0744',
+owner	=> 'www-data',
+group	=> 'www-data',
+content	=> 'I love Puppet'
 }
