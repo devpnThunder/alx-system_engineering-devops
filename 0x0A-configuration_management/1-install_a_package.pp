579d28f607d3dd9eabe97d473a6eefbc99654cb4
devpnThunder <devpthunder@gmail.com>
2024-04-24 16:14:35 +0300

    Task #1 commit #5

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install flask 2.1.0 from pip3
-package { 'flask':
-ensure => installed,
-version => '2.1.0',
+# install puppet-lint
+package{'puppet-lint':
+ensure   => '2.5.0',
 provider => 'gem'
 }
